aa3c61282c5e405abc105eef7df9017904413dcd
TransDim <alexey.kuzychenko@gmail.com>
2021-08-13 16:39:01 -0400

    Support heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent = ''
     e.preventDefault()
     const location = search.value
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = 'Error: ' + data.error
