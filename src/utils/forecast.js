5983635d5a887ba141fb4b3b9910594ced6388b4
TransDim <alexey.kuzychenko@gmail.com>
2021-08-14 14:25:08 -0400

    Add wind speed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degree out and humidity is ' + body.current.humidity + '%')
+            callback(undefined, body.current.weather_descriptions[0] + ' and wind speed is ' + body.current.wind_speed + '. It is currently ' + body.current.temperature + ' degree out and humidity is ' + body.current.humidity + '%')
         }
     })
 }
